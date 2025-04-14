61da1a6a2f0a0b8dc7402d89f8748ad7bfc9e33c
Themydee <nifetemiboy@gmail.com>
2025-04-14 13:52:34 +0100

    continuation

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -32,6 +32,51 @@ export const signup = async (req,res) => {
 
         tokenGeneratorAndCookieSetter(res, newUser._id);
     } catch (error) {
-        
+        res.status(400).json({success: false, message: error.message})
     }
+}
+
+
+export const verifyEmail = async(req, res) => {
+    const { code } = req.body
+
+    try {
+        const user = await User.findOne({
+            verificationToken: code,
+            verificationTokenExpiresAt: { $gt: Date.now()},
+        });
+
+        if (!user) {
+            console.log("Verification failed: Invalid or expired token sent");
+            return res.status(400).json({
+                success: false,
+                message: "Invalid or expired verification code"
+            });
+        }
+
+        user.isVerified = true,
+        user.verificationToken = undefined,
+        user.verificationTokenExpiresAt = undefined
+
+        await user.save();
+        // send mail construct appears here
+
+        res.status(200).json({
+            success: true,
+            message: "Email verified successfully",
+            user: {
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                isVerified: user.isVerified,
+                lastLogin: user.lastLogin
+            }
+        });
+    } catch (error) {
+        res.status(400).json({success: false, message: "Email could not be verified due to unknown reasons. That one na your wahala"}) //😅🫠🫠🫠
+    }
+}
+
+export const login = async(req, res) => {
+    
 }
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,6 +1,6 @@
 import express from "express"
 
-import { signup, login, logout, verifyEmail } from " ../controllers/auth.controller.js"
+import { signup, login, logout, verifyEmail } from "../controllers/auth.controller.js"
 
 
 const router = express.Router();
